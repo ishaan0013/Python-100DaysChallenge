f24a7e5c0e1de1878c66cf04e05dc3128158d377
Ishaan Bhardwaj <ishaanbhardwaj0013@gmail.com>
2022-12-01 21:33:10 +0530

    Commit

---
+++ Day4.py
@@ -0,0 +1,6 @@
+#How to print multiple values or strings in 1 print statement
+
+print(10+23)
+print("First","Second")
+print("Ishaan", "Bhardwaj",".My age is",22)
+print("That's it","See you tomorrow")
