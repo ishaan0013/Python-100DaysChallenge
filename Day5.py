f659e0745530da5a13597884d06114d8ba2aade8
Ishaan Bhardwaj <ishaanbhardwaj0013@gmail.com>
2022-12-02 22:35:52 +0530

    commit

---
+++ Day5.py
@@ -0,0 +1,13 @@
+# This is a comment linr
+# these line are not executed by the compiler
+'''we can also use triple single or double quotes for comments'''
+
+
+print('hello')
+# we can not use single quotes(or double quotes) inside a print statement because the compiler will not get it.
+# so for it we use 'escape sequence character'
+print('hello\'Ishaan\'')
+
+# some of the peremeters used inside the print statement are sep,end,file
+print('hello','I','am','Ishaan',sep='-',end=' bye\n')
+print('end','of','day',5,sep='-')
