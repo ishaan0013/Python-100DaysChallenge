b8f213c9bd430f1fa2e03a87f6fc24baff1fef43
Ishaan Bhardwaj <ishaanbhardwaj0013@gmail.com>
2022-12-04 23:02:20 +0530

    Commit

---
+++ Day7.py
@@ -0,0 +1,20 @@
+#calculator in python
+
+print('1 for add\n2 for sub\n3 for division\n4 for multiply')
+val = int(input())
+a=10
+b=5
+
+if val == 1:
+    print(a+b)
+elif val==2:
+    print(a-b)
+elif val==3:
+    print(a/b)
+else:
+    print(a*b)
+
+
+    
+    
+
