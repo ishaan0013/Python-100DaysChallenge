9ec5d43e0e542b75ff136cc0acd7f6dfe99e1473
Ishaan Bhardwaj <ishaanbhardwaj0013@gmail.com>
2022-12-06 23:20:16 +0530

    Commit

---
+++ Day9.py
@@ -0,0 +1,12 @@
+#typecasting in pything is of 2 types 
+# 1- explicit (Done my us)
+a = '12'
+b = '24'
+print(int(a)+int(b))
+print(type(a),type(b))
+# 2- implicit (done by python interpreter)
+c = 1.3
+d = 3
+e = c+d
+print(e)
+print(type(e))
