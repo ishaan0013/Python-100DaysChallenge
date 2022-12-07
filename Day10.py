fb0d76d25585cf4507c365b9edbd6cbee259197e
Ishaan Bhardwaj <ishaanbhardwaj0013@gmail.com>
2022-12-07 21:32:04 +0530

    Commit

---
+++ Day10.py
@@ -0,0 +1,12 @@
+#how to take input from user
+#by input() function
+
+a = input()
+print("hii",a)
+
+#but the problem is that input() funcion only take string as input
+# so we have to convert it into any other type which we want
+
+b = int(input())
+print('integer',b)
+
