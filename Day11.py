6f63dd2ac2ee8449bf7230e4bd921ff50567c670
Ishaan Bhardwaj <ishaanbhardwaj0013@gmail.com>
2022-12-08 23:44:53 +0530

    Commit

---
+++ Day11.py
@@ -0,0 +1,17 @@
+#strings
+# it is "just like array" not completely-JUST LIKE
+#we can use single or double quotes both
+name = 'Ishaan'
+print(name)
+# we can add multiple lines by triple single/double quotes
+a = """hii
+my
+name is 
+Ishaan.
+this is my 100 days
+code"""
+print(a)
+#we can also access the value of a string by its index
+print(name[0])
+#len return the length of array
+print(len(name))
