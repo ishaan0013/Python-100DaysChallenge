9955a149603ae7132c0c638c34a5ac00deb1a57f
Ishaan Bhardwaj <ishaanbhardwaj0013@gmail.com>
2022-12-09 23:20:12 +0530

    Commit

---
+++ Day12.py
@@ -0,0 +1,9 @@
+#string slicing
+
+name = "Ishaan Bhardwaj"
+
+print(name)
+print(name[0:7])
+print(name[-9:-8]) #this will print space between the 2 letters
+
+# print(name[-9:-8]) , print(name[-9:len(name)-8]) & print(name[len(name)-9:-8]) means same 
