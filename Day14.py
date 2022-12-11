efce5bdd7350e1e7532f256ce704f8b22360a79d
Ishaan Bhardwaj <ishaanbhardwaj0013@gmail.com>
2022-12-11 20:38:06 +0530

    Commit

---
+++ Day14.py
@@ -0,0 +1,10 @@
+#conditional statement
+
+a = int(input('Enter a number'))
+if a==0:
+    print('Number is zero.')
+elif a>0:
+    print('Number is greater than 0.')
+else:
+    print('Number is less than 0.')
+    
