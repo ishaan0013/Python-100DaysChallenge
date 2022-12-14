8c0c7cc25dcc19e3a8f7157a3a8fbd3f862188d8
Ishaan Bhardwaj <ishaanbhardwaj0013@gmail.com>
2022-12-14 18:38:18 +0530

    Commit

---
+++ Day17.py
@@ -0,0 +1,13 @@
+#loop
+
+for  a in range(10):
+ print(a+1)
+
+name = 'Ishaan'
+for nam in name:
+    print(nam , end=(', '))
+    
+array =  {1,2,3,4,5,6,7,8}
+print('\n')
+for ar in array:
+    print(ar,end=(' '))
