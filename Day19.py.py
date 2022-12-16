d01956784947e2554f1659953b5018669bd99f16
Ishaan Bhardwaj <ishaanbhardwaj0013@gmail.com>
2022-12-17 00:36:47 +0530

    Commit

---
+++ Day19.py.py
@@ -0,0 +1,13 @@
+#break and continue statement
+
+for i in range(15):
+  if(i==10):
+   continue
+  print("2 * ",i,"= ",i*2)
+  
+for j in range(15):
+ print("5 * ",j,"= ",j*5)
+ if(j==10):
+     break
+
+    
