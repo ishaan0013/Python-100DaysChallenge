436b9aeed96a11eed87fe671369c413487031842
Ishaan Bhardwaj <ishaanbhardwaj0013@gmail.com>
2022-12-17 21:30:44 +0530

    Commit

---
+++ Day20.py
@@ -0,0 +1,9 @@
+#python functions 
+
+def add(a,b):
+    c = a+b
+    return c
+num1= float(input("Enter first numbers you want to add: "))
+num2= float(input("Enter second numbers you want to add: "))
+result = add(num1,num2)
+print("Addition is:",result)
