aa6cab2bf1c73eb00b0cbd02af5b58c4c10e2477
Ishaan Bhardwaj <ishaanbhardwaj0013@gmail.com>
2022-12-22 15:43:19 +0530

    Commit

---
+++ Day24.py
@@ -0,0 +1,16 @@
+#tupple
+#same as list but can not be changed after (immutable)
+tup1=(1,4,56,73,"ishaan",True)
+print(type(tup1),"\n",tup1)
+
+#tup1.append(12) this gives error because this is tuple not list
+
+if 4 in tup1:
+    print("YESSS")
+else:
+    print("NOOOO")
+if 35 in tup1:
+    print("HAAANNN")
+else:
+    print("NAAHHIII")
+
