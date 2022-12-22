e850531da2bad5a7d9beb3614f8189fa9ec892a5
Ishaan Bhardwaj <ishaanbhardwaj0013@gmail.com>
2022-12-22 23:39:29 +0530

    Commit

---
+++ Day25.py
@@ -0,0 +1,11 @@
+#tuple manipulation
+#tuple are immutable
+
+tup1=(1,23,56,"ishaan",True)
+tup2=("Bhardwaj",False,53,98)
+tup3=tup1+tup2
+print(tup3)
+a=tup3.count(1)
+print(a)
+b=tup3.index(53)
+print(b)
