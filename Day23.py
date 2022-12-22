18ad1e0ef224683c2f3cf761b76a5a4b41b7b230
Ishaan Bhardwaj <ishaanbhardwaj0013@gmail.com>
2022-12-22 15:13:21 +0530

    Commit

---
+++ Day23.py
@@ -0,0 +1,14 @@
+#some of the List methods
+
+list1=[1,2,3,4,76,12,65,8,4,78,20]
+list2=[500,400,200,700,100]
+list1.append(95)
+print(list1)
+list1.reverse()
+print(list1)
+list1.sort()
+print(list1)
+list3 = list2.copy()
+print(list3)
+list1.insert(2,999)
+print(list1)
