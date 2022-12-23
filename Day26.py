76a1d9d0a8f1a49cc1779b385a403767eb5eb7c1
Ishaan Bhardwaj <ishaanbhardwaj0013@gmail.com>
2022-12-23 20:52:02 +0530

    Commit

---
+++ Day26.py
@@ -0,0 +1,8 @@
+#sets
+#Sets are used to store multiple items in a single variable.
+# set is a collection which is unordered, unchangeable*, and unindexed.
+
+set1={"Ishaan","Bhardwaj","Ishaan","age-21","India",True} #duplicate value is ignored
+print(type(set1))
+print(set1)
+
