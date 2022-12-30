8a98aa6a59be94de0bb4a22513f2debcfe024481
Ishaan Bhardwaj <ishaanbhardwaj0013@gmail.com>
2022-12-30 18:24:48 +0530

    Commit

---
+++ Day33.py
@@ -0,0 +1,8 @@
+# Dictionaries
+dic={"name":"Ishaan","Age":22,"Country":"India","Programmer":True}
+print(dic)
+print(dic.get("name"))
+print(dic["name"])
+print(dic.values())
+print(dic.keys())
+print(dic.items())
