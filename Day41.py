a2664e16ddd206a37dcbf3886cab740a3fc497e8
Ishaan Bhardwaj <ishaanbhardwaj0013@gmail.com>
2023-01-07 22:09:18 +0530

    Commit

---
+++ Day41.py
@@ -0,0 +1,13 @@
+#short-end  If Else
+#we can use a short syntax of if else for shorter problems
+
+a=10
+b=20
+if(a<b):
+    print("hello")
+else:
+    print("Bye")
+#This is the normal syntax above
+
+print("Hello") if b>a else print("Bye")
+#this is a short-end syntax
