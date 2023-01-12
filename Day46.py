1d6b48f9c159175bcce7aeaada2ecc91776dd04d
Ishaan Bhardwaj <ishaanbhardwaj0013@gmail.com>
2023-01-13 00:42:35 +0530

    Commit

--- Day46.py
+++ Day46.py
@@ -6,5 +6,13 @@ if(not os.path.exists("46 day reference")):
 if(not os.path.exists("46 day reference/By os")):
     os.mkdir("46 day reference/By os")
 os.getcwd() #this shows the current directory
-print(os.listdir("46 day reference")) #this will list all the directory 
-os.rename("46 day reference/By os","46 day reference/Very nice")#this will rename a current directory
+# print(os.listdir("46 day reference")) #this will list all the directory 
+# os.rename("46 day reference/By os","46 day reference/Very nice")#this will rename a current directory
+
+
+# os.chdir("C:\IISHHAANN\Python 100Days challenge\46 day reference\By os")
+path='C:/IISHHAANN/Python 100Days challenge/46 day reference'
+os.chdir(path)
+print(os.getcwd())
+with open("demo.txt","w") as fp:
+    pass
