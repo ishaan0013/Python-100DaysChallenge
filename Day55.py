27383ff62c7b921430c1da4b4894ad44ee92e715
Ishaan Bhardwaj <ishaanbhardwaj0013@gmail.com>
2023-01-23 19:17:42 +0530

    Commit

---
+++ Day55.py
@@ -0,0 +1,14 @@
+#Program to Check Leap Year
+
+
+year = int(input("Enter year:"))
+
+
+if (year % 400 == 0) and (year % 100 == 0):
+    print(f"{year} is a leap year")
+
+elif (year % 4 ==0) and (year % 100 != 0):
+    print(f"{year} is a leap year")
+
+else:
+    print(f"{year} is not a leap year")
