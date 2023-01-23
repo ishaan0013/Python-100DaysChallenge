a17c3e508fe91e72d3232f1cb2cb46ea9f3f4a76
Ishaan Bhardwaj <ishaanbhardwaj0013@gmail.com>
2023-01-23 21:42:02 +0530

    Commit

--- Day56.py
+++ Day56.py
@@ -7,6 +7,14 @@
 # Print True if P(x)=k. Otherwise, print False.
 
 
+#Sample input:
+# 1  4
+# x**3 + x**2 + x + 1
+
+#Sample Output:
+#True
+
+
 inp=list(map(int,input().split(" ")))
 val_x=inp[0]
 ans=inp[1]
