55af5124a839d54c3aa2d314f4bfc8eda606b38e
Ishaan Bhardwaj <ishaanbhardwaj0013@gmail.com>
2023-01-30 18:22:16 +0530

    Commit

--- Day64.py
+++ Day64.py
@@ -3,18 +3,18 @@
 
 def compute_lcm(x, y):
 
-   if x > y:
-       greater = x
-   else:
-       greater = y
+    if x > y:
+        greater = x
+    else:
+        greater = y
 
-   while(True):
-       if((greater % x == 0) and (greater % y == 0)):
-           lcm = greater
-           break
-       greater += 1
+    while(True):
+        if((greater % x == 0) and (greater % y == 0)):
+            lcm = greater
+            break
+        greater += 1
 
-   return lcm
+    return lcm
 
 num1 = int(input("Enter first Number:"))
 num2 = int(input("Enter Second Number:"))
