ec7e6de47b07ba73616f83edaaccd002051eb3fc
Ishaan Bhardwaj <ishaanbhardwaj0013@gmail.com>
2023-02-10 00:27:25 +0530

    Commit

--- Day73.py
+++ Day73.py
@@ -1,26 +1,23 @@
 #Magic/Dunder Methods
 class Employee:
 
-  def __init__(self, name):
-    self.name = name
+    def __init__(self, name):
+        self.name = name
 
-  def __len__(self):
-    i = 0
-    for c in self.name:
-      i = i + 1
-    return i
+    def __len__(self):
+        i = 0
+        for c in self.name:
+            i = i + 1
+        return i
 
-  def __str__(self):
-    return f"The name of the employee is {self.name} str"
+    def __str__(self):
+        return f"The name of the employee is {self.name} str"
     
-  def __repr__(self):
-    return f"Employee('{self.name}')"
+    def __repr__(self):
+        return f"Employee('{self.name}')"
 
-  def __call__(self):
-    print("Hey I am good")
-
-
-
+    def __call__(self):
+        print("Hey I am good")
 
 e = Employee("Ishaan")
 print(str(e))
