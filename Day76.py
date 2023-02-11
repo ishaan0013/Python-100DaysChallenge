55b6b233bcc9c6bd62684ca8955c1b03baed8542
Ishaan Bhardwaj <ishaanbhardwaj0013@gmail.com>
2023-02-11 22:57:05 +0530

    Commit

--- Day76.py
+++ Day76.py
@@ -1,2 +1,12 @@
-#Merge pdf using pyPDF
-import pypdf
+# Merge pdf using pyPDF
+
+from PyPDF2 import PdfMerger
+
+merger = PdfMerger()
+pdfs=['AI.pdf','ML.pdf']
+
+for pdf in pdfs:
+    merger.append(pdf)
+
+merger.write("merged-pdf.pdf")
+merger.close()
