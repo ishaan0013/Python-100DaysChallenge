0b91fbb120bef53a197c15a597c5d1c6fbd5ae79
Ishaan Bhardwaj <ishaanbhardwaj0013@gmail.com>
2023-02-21 22:17:09 +0530

    Commit

---
+++ Day86.py
@@ -0,0 +1,17 @@
+# walrus operator :=
+
+
+# happy = False
+# print(happy)
+
+# print(happy := True)
+
+# foods = list()
+# while True:
+#   food = input("What food do you like?: ")
+#   if food == "quit":
+#       break
+#   foods.append(food)
+foods = []
+while (food := input("What food do you like?: ")) != "quit":
+    foods.append(food)
